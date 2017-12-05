6eb1fda286e665bc7ebc57ff5e85bf1ce6aa7425
faklijta <faklijta@gmail.com>
2017-12-05 15:51:23 +0100

    feat(bcrypt) taken out from lock file

--- selection/app/controllers/omniauth_callbacks_controller.rb
+++ selection/app/controllers/omniauth_callbacks_controller.rb
@@ -2,5 +2,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def github
     @user = User.from_omniauth(request.env['omniauth.auth'])
     sign_in_and_redirect @user
+    puts request.env['omniauth.auth']
   end
+
 end
