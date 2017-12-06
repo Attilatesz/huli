3e01bcae2f389f4a0ae61dce18d39d7a9c0bc92a
Krisztian Szucs <krisztians0818@gmail.com>
2017-12-06 16:44:56 +0100

    feat(profile page): after login profile button shows user data

--- selection/app/controllers/welcome_controller.rb
+++ selection/app/controllers/welcome_controller.rb
@@ -2,4 +2,8 @@ class WelcomeController < ApplicationController
     # Krsztian's method name have to be the same --> index, DELETE this comment if it works! :)
     def index
     end
+
+    def profile
+			@user = current_user
+		end
 end
