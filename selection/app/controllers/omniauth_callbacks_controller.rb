5c6bb6ddb6a168fffb457e0c0ebe0d84888c3ad9
faklijta <faklijta@gmail.com>
2017-12-08 09:34:59 +0100

    feat(index on email)removed for causing duplicate entry error

--- selection/app/controllers/omniauth_callbacks_controller.rb
+++ selection/app/controllers/omniauth_callbacks_controller.rb
@@ -1,5 +1,7 @@
+require 'pp'
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def github
+    pp request.env['omniauth.auth']
     @user = User.from_omniauth(request.env['omniauth.auth'])
     sign_in_and_redirect @user
   end
