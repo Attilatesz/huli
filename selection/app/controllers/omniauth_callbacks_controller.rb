696730664020d9d1b7999ca9208475eeacfa941c
Kozma István <pisti_kozma@hotmail.com>
2017-12-08 11:26:13 +0100

    refactor(devise): move method for user creation from model to controller

--- selection/app/controllers/omniauth_callbacks_controller.rb
+++ selection/app/controllers/omniauth_callbacks_controller.rb
@@ -1,6 +1,20 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def github
-    @user = User.from_omniauth(request.env['omniauth.auth'])
+    @user = from_omniauth(request.env['omniauth.auth'])
     sign_in_and_redirect @user
+  end
+
+  private
+
+  def from_omniauth(auth)
+    User.where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.info.email if auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.github_handle = auth.info.nickname
+      user.full_name = auth.info.name
+      user.access_token = auth.credentials.token
+    end
   end
 end
--- selection/app/models/user.rb
+++ selection/app/models/user.rb
@@ -4,16 +4,4 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, omniauth_providers: [:github]
-
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.email = auth.info.email if auth.info.email
-      user.password = Devise.friendly_token[0, 20]
-      user.github_handle = auth.info.nickname
-      user.full_name = auth.info.name
-      user.access_token = auth.credentials.token
-    end
-  end
 end
