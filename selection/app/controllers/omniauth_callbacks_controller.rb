29ac101dfcbd8b998229d683659c7c0483bdb4d6
Tania <faklijta@gmail.com>
2017-12-08 10:00:41 +0100

    Pretty print taken out as not needed in production

--- selection/app/controllers/omniauth_callbacks_controller.rb
+++ selection/app/controllers/omniauth_callbacks_controller.rb
@@ -1,9 +1,6 @@
-require 'pp'
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def github
-    pp request.env['omniauth.auth']
     @user = User.from_omniauth(request.env['omniauth.auth'])
     sign_in_and_redirect @user
   end
-
 end
