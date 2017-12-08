57ba6f67c86da078e7ac67856a2b24bd75c9a66b
Kozma István <pisti_kozma@hotmail.com>
2017-12-08 12:57:16 +0100

    fix(limit model to omniauth): properly update schema

--- selection/db/schema.rb
+++ selection/db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171205153547) do
+ActiveRecord::Schema.define(version: 20171206110933) do
+
+  create_table "applicants", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email_address"
+    t.string "phone_number"
+    t.string "highest_education"
+    t.string "english_knowledge"
+    t.string "english_class"
+    t.string "payment_option"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
@@ -31,7 +41,7 @@ ActiveRecord::Schema.define(version: 20171205153547) do
     t.string "full_name"
     t.string "access_token"
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
   end
 
 end
