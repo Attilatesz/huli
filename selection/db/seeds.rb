9d2f04ef08a2af4263fdddfe59a771d66f1c0ec3
faklijta <faklijta@gmail.com>
2017-12-15 10:08:32 +0100

    feat(seeds) database feed from seeds

--- selection/db/seeds.rb
+++ selection/db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create([
+  {email: "faklijta@gmail.com", provider: "github", uid: "29408150", github_handle: "faklijta", full_name: "Admin", access_token: nil, admin: true},
+  {email: "attilanagy002@gmail.com", provider: "github", uid: "34302757", github_handle: "@alkyr81", full_name: "Applicant", access_token: nil, admin: false}
+])
+Applicant.create([
+  {first_name: "Attila", last_name: "Nagy", email_address: "attilanagy002@gmail.com", phone_number: "304770314", highest_education: "College/BSc/BA", english_knowledge: "Advanced"},
+  {first_name: "Cica", last_name: "Kiss", email_address: "cica@gmail.com", phone_number: "5555554", highest_education: "College/BSc/BA", english_knowledge: "Advanced"}
+])
+
