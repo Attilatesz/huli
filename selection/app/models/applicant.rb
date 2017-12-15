35a1df80ea13605a09ff908ac6d778000f857f7f
adriennzakar <adriennzakar@gmail.com>
2017-12-15 10:23:31 +0100

    feat(Application form validation)Move validation messages to en.yml

--- selection/app/models/applicant.rb
+++ selection/app/models/applicant.rb
@@ -3,30 +3,33 @@ class Applicant < ApplicationRecord
                         :last_name,
                         :email_address, :phone_number,
                         :highest_education, :english_knowledge,
-                        message: "This field can't be blank"
+                        message: I18n.t(:blank)
 
   validates_length_of :first_name, :last_name,
                       allow_blank: true,
                       minimum: 3, maximum: 35,
-                      too_short: 'Minimum 3 characters'
+                      message: I18n.t(:too_short)
 
   validates_length_of :phone_number,
                       allow_blank: true, in: 10..15,
-                      too_short: 'Minimum 3 characters'
+                      message: I18n.t(:too_short)
 
   validates_presence_of :english_class, :payment_option,
-                        message: 'Please choose one of the options'
+                        message: I18n.t(:select)
 
   validates_format_of :first_name, :last_name,
-                      allow_blank: true, with: /\A[a-zA-Z]+\z/,
-                      message: 'Only letters allowed'
+                      allow_blank: true, 
+                      with: /\A[a-zA-Z]+\z/,
+                      message: I18n.t(:only_letters)
 
   validates_numericality_of :phone_number,
-                            allow_blank: true, only_integer: true,
-                            message: 'Only numbers allowed'
+                            allow_blank: true,
+                            only_integer: true,
+                            message: I18n.t(:only_numbers)
 
   validates_format_of :email_address,
-                      allow_blank: true,
                       with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i,
-                      message: "The format of the e-mail address isn't correct"
+                      allow_blank: true,
+                      message: I18n.t(:email_format)
+
 end
