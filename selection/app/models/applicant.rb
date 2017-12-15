5b4f635d384c3530a2ce40459f17b5241a2073d1
adriennzakar <adriennzakar@gmail.com>
2017-12-15 14:54:55 +0100

    feat(models)Add applicants to users

--- selection/app/models/applicant.rb
+++ selection/app/models/applicant.rb
@@ -1,4 +1,6 @@
 class Applicant < ApplicationRecord
+  belongs_to :user
+
   validates_presence_of :first_name,
                         :last_name,
                         :email_address, :phone_number,
--- selection/app/models/user.rb
+++ selection/app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_one :cv, dependent: :destroy
   has_one :profile_picture, dependent: :destroy
+  has_one :applicant, dependent: :destroy
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
