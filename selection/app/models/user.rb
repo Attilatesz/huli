61d20661ddcc7188c47e757d2a68626069e7d15c
adriennzakar <adriennzakar@gmail.com>
2017-12-21 10:42:36 +0100

    style(user model)style

--- selection/app/models/user.rb
+++ selection/app/models/user.rb
@@ -6,20 +6,17 @@ class User < ApplicationRecord
   devise :rememberable, :trackable,
          :omniauthable, omniauth_providers: [:github, :google_oauth2]
 
-
-
   def self.from_omniauth(auth)
-   User.where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-     user.provider = auth.provider
-     user.uid = auth.uid
-     user.email = auth.info.email if auth.info.email
-     user.access_token = auth.credentials.token
-     user.admin = user.is_admin(auth.info.email)
-   end
+    User.where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    user.provider = auth.provider
+    user.uid = auth.uid
+    user.email = auth.info.email if auth.info.email
+    user.access_token = auth.credentials.token
+    user.admin = user.is_admin(auth.info.email)
+    end
   end
 
- def is_admin(email)
-   email.split("@").last == "gmail.com"
- end
-
+  def is_admin(email)
+    email.split('@').last == 'gmail.com'
+  end
 end
