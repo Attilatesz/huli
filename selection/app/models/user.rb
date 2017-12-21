6f79e1183774df65f0bcfc07f5ac0ff2e15edde1
adriennzakar <adriennzakar@gmail.com>
2017-12-21 10:35:42 +0100

    fix(user model)delete unnecessary fieds of user

--- selection/app/models/user.rb
+++ selection/app/models/user.rb
@@ -13,8 +13,6 @@ class User < ApplicationRecord
      user.provider = auth.provider
      user.uid = auth.uid
      user.email = auth.info.email if auth.info.email
-     user.github_handle = auth.info.nickname
-     user.full_name = auth.info.name
      user.access_token = auth.credentials.token
      user.admin = user.is_admin(auth.info.email)
    end
