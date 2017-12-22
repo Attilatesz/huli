9885717b441ffdffbab63aaa212766461ba0e476
Krisztian Szucs <krisztians0818@gmail.com>
2017-12-22 14:03:00 +0100

    refactor(admins-helper): replaced sub to gsub

--- selection/app/helpers/admins_helper.rb
+++ selection/app/helpers/admins_helper.rb
@@ -7,7 +7,7 @@ module AdminsHelper
   def status_button(status)
     case status
     when "nothing_uploaded"
-      content_tag(:div, status.sub('_', ' ').capitalize, class: 'btn-static btn-warning')
+      content_tag(:div, status.gsub('_', ' ').capitalize, class: 'btn-static btn-warning')
     when "cv_pp_approved"
       content_tag(:div, status.gsub('_', ' ').capitalize, class: 'btn btn-primary')
     end
