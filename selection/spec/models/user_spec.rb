159ae4a15a5d7316e5e5df3eee27a50a1c2330fe
faklijta <faklijta@gmail.com>
2018-01-02 17:53:37 +0100

    feat(user model valid test ok

--- selection/spec/models/user_spec.rb
+++ selection/spec/models/user_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe User, type: :model do
   it 'has a valid factory' do
     expect(create :user).to be_valid
   end
+
+  describe 'invalid' do
+    it 'when no email present' do
+      expect(build :user, email: nil).to be_invalid
+    end
+  end
 end
