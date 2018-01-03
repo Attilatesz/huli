22d59f82bbe7112e76fd029a8cd846459e283826
faklijta <faklijta@gmail.com>
2018-01-03 16:20:45 +0100

    feat(applicant validation) tested with missing data

--- selection/spec/factories/applicant.rb
+++ selection/spec/factories/applicant.rb
@@ -1,12 +1,12 @@
 FactoryBot.define do
   factory :applicant do
-    first_name "John"
-    last_name "Doe"
-    email_address "John@Doe.com"
-    phone_number  "0123456789"
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    email_address { Faker::Internet.email }
+    phone_number  { Faker::Number.number(12)}
     highest_education "BA"
     english_knowledge "Basic"
-    english_class "yes"
+    english_class { Faker::Boolean.boolean }
     payment_option "prepaid"
   end
 end
