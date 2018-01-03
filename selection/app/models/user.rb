b9d29168710a8408213ef403d9c5eb42ab3e5d4b
faklijta <faklijta@gmail.com>
2018-01-03 09:39:12 +0100

    refact(email presence if) taken out

--- selection/app/models/user.rb
+++ selection/app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
     User.where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
     user.provider = auth.provider
     user.uid = auth.uid
-    user.email = auth.info.email if auth.info.email
+    user.email = auth.info.email
     user.access_token = auth.credentials.token
     user.admin = user.is_admin(auth.info.email)
     end
