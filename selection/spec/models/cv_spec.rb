c5102b522b8d063b9910e42ba7f198ba9dd7eeb3
adriennzakar <adriennzakar@gmail.com>
2018-01-04 11:17:14 +0100

    (test)Add cv-pp invalid tests

--- selection/spec/models/cv_spec.rb
+++ selection/spec/models/cv_spec.rb
@@ -1,8 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Cv, type: :model do
-  it 'is valid with uploaded cv' do
+  it 'has valid factory' do
     cv = build :cv
     expect(cv).to be_valid
+  end
+  describe 'invalid' do
+    it 'is not valid without applicant' do
+      expect(build :cv, applicant: nil).to be_invalid
+    end
+    it'is not valid without cv' do
+      expect(build :cv, cv: nil).to be_invalid
+    end
   end
 end
--- selection/spec/models/profile_picture_spec.rb
+++ selection/spec/models/profile_picture_spec.rb
@@ -5,4 +5,12 @@ RSpec.describe ProfilePicture, type: :model do
     profile_picture = build :profile_picture
     expect(profile_picture).to be_valid
   end
+  describe 'invalid' do
+    it 'is not valid without applicant' do
+      expect(build :profile_picture, applicant: nil).to be_invalid
+    end
+    it'is not valid without image' do
+      expect(build :profile_picture, image: nil).to be_invalid
+    end
+  end
 end
