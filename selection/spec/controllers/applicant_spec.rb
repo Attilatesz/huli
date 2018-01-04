7b795367d6204196c86308677f878f9b276fabfb
faklijta <faklijta@gmail.com>
2018-01-04 14:19:17 +0100

    feat(applicant controller) test on new complete

--- selection/spec/controllers/applicant_spec.rb
+++ selection/spec/controllers/applicant_spec.rb
@@ -2,11 +2,42 @@ require 'rails_helper'
 
 RSpec.describe ApplicantsController, type: :controller do
 
+  describe 'GET new' do
+    subject { get :new }
+
+    it 'renders the applicants new template' do
+      expect(subject).to render_template('applicants/new')
+    end
+
+    it 'returns a 200 status code' do
+      get :new
+      expect(response).to have_http_status(200)
+    end
+
+  end
+#is this the same as above?
   context 'GET new' do
 
-    it 'creates a new applicant' do
+    it 'assign the application form' do
       get :new
-      expect(assigns(:applicant)).to be_a_new(Applicant)
+      expect(assigns :applicant).to be_a_new(Applicant)
     end
+
   end
+
+  context 'POST create' do
+
+    it 'saves new applicant to database' do
+      create :applicant
+      expect(Applicant.count).to eq 1
+    end
+
+    # fails with error: Expected response to be a <3xx: redirect>, but was a <200: OK>
+    # it 'redirects to welcome profile path' do
+    #   expect(build :applicant).to redirect_to(welcome_profile_path)
+    # end
+
+  end
+
+
 end
