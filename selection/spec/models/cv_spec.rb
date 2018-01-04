30436c793be60e848b0c194a171aac581ea3a4c5
adriennzakar <adriennzakar@gmail.com>
2018-01-04 14:52:50 +0100

    style

--- selection/spec/models/cv_spec.rb
+++ selection/spec/models/cv_spec.rb
@@ -5,24 +5,31 @@ RSpec.describe Cv, type: :model do
     cv = build :cv
     expect(cv).to be_valid
   end
+
   it do
     should validate_presence_of(:cv).with_message('Please upload a file')
   end
+
   it do
     should belong_to(:applicant)
   end
+
   describe 'invalid' do
     it 'is not valid without applicant' do
       expect(build :cv, applicant: nil).to be_invalid
     end
+
     it'is not valid without cv' do
       expect(build :cv, cv: nil).to be_invalid
     end
+
     it do
       should_not allow_values('test.jpg', 'test.doc').for(:cv)
     end
+
     it do
       should_not allow_value('test.pdf').for(:cv)
     end
+    
   end
 end
--- selection/spec/models/profile_picture_spec.rb
+++ selection/spec/models/profile_picture_spec.rb
@@ -5,18 +5,23 @@ RSpec.describe ProfilePicture, type: :model do
     profile_picture = build :profile_picture
     expect(profile_picture).to be_valid
   end
+
   it do
     should validate_presence_of(:image).with_message('Please upload a file')
   end
+
   it do
     should belong_to(:applicant)
   end
+
   describe 'invalid' do
     it 'is not valid without applicant' do
       expect(build :profile_picture, applicant: nil).to be_invalid
     end
+
     it'is not valid without image' do
       expect(build :profile_picture, image: nil).to be_invalid
     end
+    
   end
 end
