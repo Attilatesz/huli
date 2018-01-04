0becf5fe47d34e91ba7ee0ca106bcfcfa993bafa
adriennzakar <adriennzakar@gmail.com>
2018-01-04 13:41:36 +0100

    (test)Add shoulda-matchers, write tests with it

--- selection/spec/models/profile_picture_spec.rb
+++ selection/spec/models/profile_picture_spec.rb
@@ -5,6 +5,12 @@ RSpec.describe ProfilePicture, type: :model do
     profile_picture = build :profile_picture
     expect(profile_picture).to be_valid
   end
+  it do
+    should validate_presence_of(:image).with_message('Please upload a file')
+  end
+  it do
+    should belong_to(:applicant)
+  end
   describe 'invalid' do
     it 'is not valid without applicant' do
       expect(build :profile_picture, applicant: nil).to be_invalid
