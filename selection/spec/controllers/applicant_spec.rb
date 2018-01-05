c0a5d98614d0589095d1aefe384345b52f16c83c
faklijta <faklijta@gmail.com>
2018-01-05 13:04:24 +0100

    feat(with invalid attributes) endpoints tested

--- selection/spec/controllers/applicant_spec.rb
+++ selection/spec/controllers/applicant_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe ApplicantsController, type: :controller do
   let! (:user) {create :user}
 
   describe 'GET new' do
-    subject { get :new }
 
+    subject { get :new }
     it 'renders the applicants new template' do
       expect(subject).to render_template('applicants/new')
     end
@@ -15,37 +15,54 @@ RSpec.describe ApplicantsController, type: :controller do
       expect(response).to have_http_status(200)
     end
 
-  end
-
-  context 'GET new' do
-
-    it 'assign the application form' do
+    it 'assign a new applicant' do
       get :new
       expect(assigns :applicant).to be_a_new(Applicant)
     end
-
   end
 
-  context 'POST applicants' do
+  describe 'POST applicants' do
 
-    it 'increases the number of applicant by one' do
-      expect do
+    context 'with valid attributes' do
+
+      it 'increases the number of applicant by one' do
+        expect do
+          sign_in user
+          post :create, params: { applicant: {first_name: "Edes", last_name: "Anna", email_address: "edesanna@hotmail.com",
+          phone_number: "063055555555", highest_education: "College/BSc/BA", english_knowledge: "Advanced",
+          english_class: "English", payment_option: "Prepayment_headhunting"}}
+        end.to change(Applicant, :count).by(1)
+      end
+
+      it 'responds with 302 and redirects to ' do
         sign_in user
         post :create, params: { applicant: {first_name: "Edes", last_name: "Anna", email_address: "edesanna@hotmail.com",
         phone_number: "063055555555", highest_education: "College/BSc/BA", english_knowledge: "Advanced",
         english_class: "English", payment_option: "Prepayment_headhunting"}}
-      end.to change(Applicant, :count).by(1)
+        expect(response.status).to eq(302)
+        expect(response).to redirect_to(welcome_profile_path)
+      end
     end
 
-    # fails with error: Expected response to be a <3xx: redirect>, but was a <200: OK>
-    # it 'redirects to welcome profile path' do
-    #   expect(build :applicant).to redirect_to(welcome_profile_path)
-    # end
+    context 'with invalid attributes' do
 
-    # it 'responds with error if email missing' do
-    #   expect(build :applicant, email_address: nil).to raise_error "This field can't be blank"
-    # end
+      it 'responds with 200 and renders the form again' do
+        sign_in user
+        post :create, params: { applicant: {email_address: 'edesanna@hotmail.com'}}
+        expect(response.status).to eq(200)
+        expect(response).to render_template('applicants/new')
+      end
+    end
+
+      it 'does not increases the number of applicant' do
+        expect do
+          sign_in user
+          post :create, params: { applicant: {first_name: "Edes"}}
+        end.not_to change(Applicant, :count)
+      end
+
   end
+
 
 
 end
