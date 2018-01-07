05be94b05364890dbc415d80792c93d3c8869fc8
adriennzakar <adriennzakar@gmail.com>
2018-01-07 20:18:37 +0100

    refactor(spec)Refactor cv controller tests

--- selection/spec/controllers/cv_spec.rb
+++ selection/spec/controllers/cv_spec.rb
@@ -34,34 +34,28 @@ describe 'POST create' do
 
   it 'it returns status code 302 and redirect' do
     sign_in user
-    user.create_applicant( first_name: "Kiss", last_name: "Krisztina", email_address: "KKriszti@hotmail.com",
-    phone_number: "06308888888", highest_education: "College/BSc/BA", english_knowledge: "Advanced",
-    english_class: "English", payment_option: "Prepayment_headhunting", user_id: user.id)
+    user.create_applicant(attributes_for(:applicant))
     cv = fixture_file_upload('pdf-sample.pdf', 'application/pdf')
-    post :create, params: { cv: { cv: cv} }
+    post :create, params: { cv: { cv: cv } }
     expect(response).to have_http_status(302)
     expect(response).to redirect_to('/')
     expect(controller).to set_flash[:success]
   end
 
-  it 'increases the number of  by one' do
+  it 'increases the number of Cv by one' do
     expect do
       sign_in user
-      user.create_applicant( first_name: "Kiss", last_name: "Krisztina", email_address: "KKriszti@hotmail.com",
-      phone_number: "06308888888", highest_education: "College/BSc/BA", english_knowledge: "Advanced",
-      english_class: "English", payment_option: "Prepayment_headhunting", user_id: user.id)
+      user.create_applicant(attributes_for(:applicant))
       cv = fixture_file_upload('pdf-sample.pdf', 'application/pdf')
-      post :create, params: { cv: { cv: cv} }
+      post :create, params: { cv: { cv: cv } }
     end.to change(Cv, :count).by(1)
   end
 
   it 'it returns status code 200 and reload page if the format not valid' do
     sign_in user
-    user.create_applicant( first_name: "Kiss", last_name: "Krisztina", email_address: "KKriszti@hotmail.com",
-    phone_number: "06308888888", highest_education: "College/BSc/BA", english_knowledge: "Advanced",
-    english_class: "English", payment_option: "Prepayment_headhunting", user_id: user.id)
-    #cv = fixture_file_upload('TestWordDoc.doc', 'application/doc')
-    #post :create, params: { cv: { cv: cv} }
+    user.create_applicant(attributes_for(:applicant))
+    cv = fixture_file_upload('TestWordDoc.doc', 'application/doc')
+    post :create, params: { cv: { cv: cv } }
     expect(response).to have_http_status(200)
     expect(subject).to render_template('cvs/new')
   end
