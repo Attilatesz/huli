4873495649f450ab3f6e2b295ebb6c3fe3047e9f
adriennzakar <adriennzakar@gmail.com>
2018-01-07 23:49:47 +0100

    refactor(spec)Refactor cv controller tests

--- selection/spec/controllers/cv_spec.rb
+++ selection/spec/controllers/cv_spec.rb
@@ -5,36 +5,30 @@ RSpec.describe CvsController, type: :controller do
 
 describe 'GET new' do
 
-  it 'returns status code 200 if user signed in' do
+  subject { get :new }
+  it 'returns status code 200 and renderscv/new' do
     sign_in user
     get :new
     expect(response).to have_http_status(200)
-  end
-
-  subject { get :new }
-  it 'renders the cv new if user signed in' do
-    sign_in user
     expect(subject).to render_template('cvs/new')
   end
 
-  it 'redirect to root without user' do
-    get :new
-    expect(response).to redirect_to ('/')
-  end
-
-  it 'returns status code 302 without user' do
+  it 'returns status code 302 and redirect to root without user' do
     get :new
     expect(response).to have_http_status(302)
+    expect(response).to redirect_to('/')
   end
 end
 
 describe 'POST create' do
 
-  subject { get :create}
-
-  it 'it returns status code 302 and redirect' do
+  before(:each) do
     sign_in user
     user.create_applicant(attributes_for(:applicant))
+  end
+
+  subject { get :create}
+  it 'it returns status code 302 and redirect to root' do
     cv = fixture_file_upload('pdf-sample.pdf', 'application/pdf')
     post :create, params: { cv: { cv: cv } }
     expect(response).to have_http_status(302)
@@ -42,16 +36,14 @@ describe 'POST create' do
     expect(controller).to set_flash[:success]
   end
 
-  it 'increases the number of Cv by one' do
+  it 'increases the number of Cvs by one' do
     expect do
-      sign_in user
-      user.create_applicant(attributes_for(:applicant))
       cv = fixture_file_upload('pdf-sample.pdf', 'application/pdf')
       post :create, params: { cv: { cv: cv } }
     end.to change(Cv, :count).by(1)
   end
 
-  it 'it returns status code 200 and reload page if the format not valid' do
+  it 'it returns status code 200 and reload page if the file format not valid' do
     sign_in user
     user.create_applicant(attributes_for(:applicant))
     cv = fixture_file_upload('TestWordDoc.doc', 'application/doc')
@@ -62,12 +54,14 @@ describe 'POST create' do
 end
 
 describe 'GET edit' do
+
   before(:each) do
     sign_in user
     user.create_applicant(attributes_for(:applicant))
+
   end
     subject { get :edit }
-    it 'returns status code 200 and renders the page' do
+    it 'returns status code 200 and renders the cv/edit' do
       user.applicant.create_cv(attributes_for(:cv))
       get :edit
       expect(response).to have_http_status(200)
