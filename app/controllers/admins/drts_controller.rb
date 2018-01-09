3b9be4b101ee0146768e79bd2fd1231c22a78747
faklijta <faklijta@gmail.com>
2018-01-09 14:11:31 +0100

    feat(flash message) on successfull save

--- app/controllers/admins/drts_controller.rb
+++ app/controllers/admins/drts_controller.rb
@@ -6,6 +6,18 @@ class Admins::DrtsController < Admins::AdminController
 
   def create
     @drt = Drt.create(drt_params)
+    if @drt.errors.any?
+      render 'new'
+    else
+      flash[:success] = I18n.t('flash.drt_success')
+      redirect_to root_path
+    end
   end
+
+private
+
+def drt_params
+  params.require(:drt).permit(:drt_hash)
+end
 
 end
