ef26ced1e8a0da8c1eeb5585519226573058db64
adriennzakar <adriennzakar@gmail.com>
2018-01-09 22:37:05 +0100

    feat(comments controller)Add create and new methods

---
+++ app/controllers/admins/comments_controller.rb
@@ -0,0 +1,27 @@
+class Admins::CommentsController < Admins::AdminController
+  before_action :find_commentable
+
+      def new
+        @comment = Comment.new
+      end
+
+      def create
+        @comment = @commentable.comments.new comment_params
+
+        if @comment.save
+          redirect_to :back, notice: 'Your comment was successfully posted!'
+        else
+          redirect_to :back, notice: "Your comment wasn't posted!"
+        end
+      end
+
+      private
+
+      def comment_params
+        params.require(:comment).permit(:body)
+      end
+
+      def find_commentable
+        @commentable = Applicant.find_by_id(params[:id])
+      end
+end
