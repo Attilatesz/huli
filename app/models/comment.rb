45afcfdd4a87728215f18d11487efae877a5a229
Krisztian Szucs <krisztians0818@gmail.com>
2018-01-09 13:36:33 +0100

    feat(comment model): comment model is tested

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true
+  validates_presence_of :commenter, :body
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'valid' do
+    it 'has a valid factory' do
+      expect(build :user).to be_valid
+    end
+  end
+
+  describe 'invalid' do
+    it 'has no commenter given' do
+      expect(build :comment, commenter: nil).to be_invalid
+    end
+
+    it 'has no text body given' do
+      expect(build :comment, body: nil).to be_invalid
+    end
+  end
 end
