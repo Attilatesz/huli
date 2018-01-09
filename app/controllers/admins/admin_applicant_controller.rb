f7f75f6bbcc58d303882cd423917f1fcba0ba4e7
adriennzakar <adriennzakar@gmail.com>
2018-01-09 23:24:25 +0100

    feat(views)Add comment form

--- app/controllers/admins/admin_applicant_controller.rb
+++ app/controllers/admins/admin_applicant_controller.rb
@@ -14,6 +14,7 @@ class Admins::AdminApplicantController < Admins::AdminController
 
   def show
     @applicant = Applicant.find(params[:id])
+    @comment = Comment.new
   end
 
   def search
--- app/controllers/admins/comments_controller.rb
+++ app/controllers/admins/comments_controller.rb
@@ -1,27 +1,27 @@
 class Admins::CommentsController < Admins::AdminController
   before_action :find_commentable
 
-      def new
-        @comment = Comment.new
-      end
+  def new
+    @comment = Comment.new
+  end
 
-      def create
-        @comment = @commentable.comments.new comment_params
+  def create
+    @comment = @commentable.comments.create(comment_params)
 
-        if @comment.save
-          redirect_to :back, notice: 'Your comment was successfully posted!'
-        else
-          redirect_to :back, notice: "Your comment wasn't posted!"
-        end
-      end
+    if @comment.save
+      redirect_to :back, notice: 'Your comment was successfully posted!'
+    else
+      redirect_to :back, notice: "Your comment wasn't posted!"
+    end
+  end
 
-      private
+  private
 
-      def comment_params
-        params.require(:comment).permit(:body)
-      end
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 
-      def find_commentable
-        @commentable = Applicant.find_by_id(params[:id])
-      end
+  def find_commentable
+    @commentable = Applicant.find_by_id(params[:id])
+  end
 end
