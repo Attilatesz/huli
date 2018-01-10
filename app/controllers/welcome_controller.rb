2b3ae449ac10dcff949a93c10d8851cbb7b8e8ce
Zágon Kristóf <tofi.zagon@gmail.com>
2018-01-10 14:33:38 +0100

    sending mail to letter_opener again

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,7 @@ class WelcomeController < ApplicationController
     end
 
     def profile
+      ExampleMailer.sample_email(Applicant.first).deliver
       redirect_to root_path unless current_user
     end
 end
