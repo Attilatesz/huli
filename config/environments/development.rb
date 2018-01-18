779c73d77a12d91e278270209552ce3047328d30
Zágon Kristóf <tofi.zagon@gmail.com>
2018-01-18 09:52:46 +0100

    smtp_settings removed from development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,14 +33,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
 
-  config.action_mailer.smtp_settings = {
-    address: "email-smtp.eu-west-1.amazonaws.com",
-    port: 587,
-    user_name: ENV["SES_SMTP_USERNAME"],
-    password: ENV["SES_SMTP_PASSWORD"],
-    authentication: :login,
-    enable_starttls_auto: true
-  }
+
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
